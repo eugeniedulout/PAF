04e951171426f9e934742ac083e8d1dac4ccae1e
« Lucas » <jeanlucas100100@gmail.com>
2021-06-30 16:01:23 +0200

    correction syntaxe

--- Codes_separes/CodeFlou.js
+++ Codes_separes/CodeFlou.js
@@ -7,13 +7,13 @@ uniform : [{name: 'blur_matrice', default: "[1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 
 {name: 'blur_step_h', default: "1.0/height", description : "pas des pixels en hauteur"},
 {name: 'blur_offset', default: "[(-blur_step_w, -blur_step_h), (0.0, -blur_step_h), (blur_step_w, -blur_step_h), (-blur_step_w, 0.0), (0.0,0.0), (blur_step_w, 0.0), (-blur_step_w, blur_step_h), (0.0, blur_step_h), (blur_step_w, blur_step_h)], description: "offset des pixels"}]
 
-code : 'vec4 vid = vec4(0.0, 0.0, 0.0, 1.0);
+code : `vec4 vid = vec4(0.0, 0.0, 0.0, 1.0);
   	int i = 0;
    	for (i=0; i<9; i++) {
         	vec4 tmp = texture2D(inTexture, inTextureCoord + blur_offset[i]);
         	vid.rgb += tmp.rgb * vec3(blur_matrice[i]);
      	}
-     	vid.rgb /= 9.0;
+     	vid.rgb /= 9.0;`
 
 export { name, uniforms, code };
 
--- Codes_separes/NoirBlanc.js
+++ Codes_separes/NoirBlanc.js
@@ -2,8 +2,8 @@
 
 name : "BAndW"
 uniform : [{name: 'BAndW_seuil', default: 'width*(nb_frames%100)/100', description: "largeur jusqu'à laquelle appliquer le filtre"}]
-code : 'if (gl_FragCoord.x > seuil)
-		vid.rgb = vec3((vid.r + vid.g + vid.b)/3.0);'
+code : `if (gl_FragCoord.x > seuil)
+		vid.rgb = vec3((vid.r + vid.g + vid.b)/3.0);`
 
 export { name, uniforms, code };
 
